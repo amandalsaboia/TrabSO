47f9b6e9b50bd10c7ed8c10523e7da8db6939c59
Leonardo Silveira <sombriks@gmail.com>
2017-05-18 18:16:22 -0300

    adicionando imagens dos bonecos

---
+++ src/edu/ifce/sistops/Loader.java
@@ -0,0 +1,15 @@
+package edu.ifce.sistops;
+
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public enum Loader {
+	INSTANCE;
+	public BufferedImage assetImg(String name) throws Exception {
+		try (InputStream in = Loader.class.getResourceAsStream(name)) {
+			return ImageIO.read(in);
+		}
+	}
+}
